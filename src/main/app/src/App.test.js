902bd17d363a4db9af079e86b46112d9fa383d40
Arpit Khandelwal <arkhande@indiana.edu>
2017-02-27 20:37:05 -0500

    Resolved merge conflicts

--- src/main/app/src/App.test.js
+++ src/main/app/src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducers';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const mockStore = createStore(reducer);
+  ReactDOM.render(
+    <Provider de={mockStore}>
+      <App />
+    </Provider>,div);
 });
--- src/main/app/src/index.js
+++ src/main/app/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import App from './App';
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import createLogger from 'redux-logger';
-import appReducer from './reducers';
+import reducer from './reducers';
 
-const logger = createLogger();
-let middleware = applyMiddleware(thunk, logger());
-let store = createStore(appReducer, middleware);
+const createStoreWithMiddleware = applyMiddleware(createLogger(),thunkMiddleware)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 ReactDOM.render(
   <Provider store={store} >
