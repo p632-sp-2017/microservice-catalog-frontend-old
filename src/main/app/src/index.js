715956ea085200024b78d60de549672b03db01f4
Arpit Khandelwal <arkhande@indiana.edu>
2017-02-27 22:55:04 -0500

    Use react router for Single Page Applications

--- src/main/app/src/index.js
+++ src/main/app/src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import App from './App';
+import createLogger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { Router , Route , IndexRoute, browserHistory } from 'react-router';
+
 import './index.css';
-import createLogger from 'redux-logger';
 import reducer from './reducers';
+import App from './App';
 
 const createStoreWithMiddleware = applyMiddleware(createLogger(),thunkMiddleware)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
 ReactDOM.render(
   <Provider store={store} >
-      <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={App}/>
+        <Route path="addService" component={App}/>
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
